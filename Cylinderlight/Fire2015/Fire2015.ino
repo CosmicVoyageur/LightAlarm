68b8a8ab64f662eba691225d67cf600f313cde66
Rian Finnegan <flanagan89@gmail.com>
2015-09-25 19:51:34 +1000

    Added color pallet with grid enabled

--- Cylinderlight/Fire2015/Fire2015.ino
+++ Cylinderlight/Fire2015/Fire2015.ino
@@ -5,8 +5,8 @@
 #define CHIPSET     NEOPIXEL
 #define NUM_LEDS    256
 
-#define BRIGHTNESS  100
-#define FRAMES_PER_SECOND 1000
+#define BRIGHTNESS  250
+#define FRAMES_PER_SECOND 60
 
 
 CRGB leds_plus_safety_pixel[ NUM_LEDS + 1];
@@ -27,7 +27,6 @@ const bool    kMatrixSerpentineLayout = true;
 void setup() {
   Serial.begin(115200);
   delay(3000); // sanity delay
-  //FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
    FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS);
   FastLED.setBrightness( BRIGHTNESS );
 }
@@ -40,7 +39,7 @@ void loop()
   Fire2012(); // run simulation frame
   
   FastLED.show(); // display this frame
- // FastLED.delay(1000 / FRAMES_PER_SECOND);
+  FastLED.delay(1000 / FRAMES_PER_SECOND);
 }
 
 
@@ -75,12 +74,12 @@ void loop()
 // COOLING: How much does the air cool as it rises?
 // Less cooling = taller flames.  More cooling = shorter flames.
 // Default 50, suggested range 20-100 
-#define COOLING  150
+#define COOLING  30
 
 // SPARKING: What chance (out of 255) is there that a new spark will be lit?
 // Higher chance = more roaring fire.  Lower chance = more flickery fire.
 // Default 120, suggested range 50-200.
-#define SPARKING 120
+#define SPARKING 130
 
 
 void Fire2012()
@@ -95,19 +94,19 @@ void Fire2012()
     }
  // Serial.println("Step 2");
     // Step 2.  Heat from each cell drifts 'up' and diffuses a little
-    for( int x= nx - 1; x>= 2; x--) {
-       for( int y= ny - 1; y>= 2; y--) {
+    for( int x= nx - 1; x>= 0; x--) {
+       for( int y= ny - 1; y>= 1; y--) {
       //heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
-      
-        int k = XYsafe(x,y); int k1 = XYsafe(x,y-1); int k11 = XYsafe(x+1,y-1);int k12 = XYsafe(x-1,y-1);int k2 = XYsafe(x,y-2); 
-        heat[k] = (heat[k1] + heat[k11] + + heat[k12] + heat[k2] + heat[k2] ) / 5;
+        int k = XYsafe(x,y);int k0 = XYsafe(x-1,y);int k01 = XYsafe(x+1,y); int k1 = XYsafe(x,y-1); int k11 = XYsafe(x+1,y-1);int k12 = XYsafe(x-1,y-1);int k2 = XYsafe(x,y-2); 
+        heat[k] = (2 * heat[k1] + heat[k0]+ heat[k01] + heat[k11] + heat[k12] + 2 * heat[k2] ) / ( 3 * (y+1));
        }
     }
  //   Serial.println("Step 3");
     // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
     if( random8() < SPARKING ) {
-      int x = random8(1, nx);
-      int q = XYsafe(x,1);
+      int x = random8(0, nx);
+      int y = random8(0,1);
+      int q = XYsafe(x,y);
       heat[q] = qadd8( heat[q], random8(160,255) );
     }
 //   Serial.println("Step 4");
@@ -118,60 +117,6 @@ void Fire2012()
 }
 
 
-
-
-// Set 'kMatrixSerpentineLayout' to false if your pixels are 
-// laid out all running the same way, like this:
-//
-//     0 >  1 >  2 >  3 >  4
-//                         |
-//     .----<----<----<----'
-//     |
-//     5 >  6 >  7 >  8 >  9
-//                         |
-//     .----<----<----<----'
-//     |
-//    10 > 11 > 12 > 13 > 14
-//                         |
-//     .----<----<----<----'
-//     |
-//    15 > 16 > 17 > 18 > 19
-//
-// Set 'kMatrixSerpentineLayout' to true if your pixels are 
-// laid out back-and-forth, like this:
-//
-//     0 >  1 >  2 >  3 >  4
-//                         |
-//                         |
-//     9 <  8 <  7 <  6 <  5
-//     |
-//     |
-//    10 > 11 > 12 > 13 > 14
-//                        |
-//                        |
-//    19 < 18 < 17 < 16 < 15
-//
-// Bonus vocabulary word: anything that goes one way 
-// in one row, and then backwards in the next row, and so on
-// is call "boustrophedon", meaning "as the ox plows."
-
-
-// This function will return the right 'led index number' for 
-// a given set of X and Y coordinates on your matrix.  
-// IT DOES NOT CHECK THE COORDINATE BOUNDARIES.  
-// That's up to you.  Don't pass it bogus values.
-//
-// Use the "XY" function like this:
-//
-//    for( uint8_t x = 0; x < kMatrixWidth; x++) {
-//      for( uint8_t y = 0; y < kMatrixHeight; y++) {
-//      
-//        // Here's the x, y to 'led index' in action: 
-//        leds[ XY( x, y) ] = CHSV( random8(), 255, 255);
-//      
-//      }
-//    }
-//
 //
 uint16_t XY( uint8_t x, uint8_t y)
 {
@@ -196,12 +141,12 @@ uint16_t XY( uint8_t x, uint8_t y)
 }
 
 
-
+// actually wraps
 uint16_t XYsafe( uint8_t x, uint8_t y)
 {
   //Serial.print("gotx: ");  Serial.print(x);  Serial.print("  goty: ");Serial.println(y);
   
-  if( x >= nx) return -1;
-  if( y >= ny) return -1;
+  if( x >= nx) return -1 + (nx - x); // extra wrapping
+  if( y >= ny) return -1 + (ny - y);
   return XY(x,y);
 }
